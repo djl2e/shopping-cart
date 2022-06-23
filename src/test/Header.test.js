7602aa961666e5924c41114f6393244068335a80
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-23 14:16:42 -0400

    moved BrowserRouter location

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from '../components/Header';
+
+describe('Header component', () => {
+  it('container with classname header', () => {
+    const { container } = render(<Header />);
+    expect(container.classList).toContain('header');
+  });
+  it('header has two components: logo and links', () => {
+    const { container } = render(<Header />);
+    expect(container.children).toHaveLength(2);
+  });
+  it('nav contains 4 options', () => {
+    render(<Header />);
+    expect(screen.getByRole('navigation').children).toHaveLength(4);
+  });
+});
