3f4bd437ee8d27805a059a06e6c9b0a10a6a7a2b
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-22 20:38:13 -0400

    folder structure to src

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
