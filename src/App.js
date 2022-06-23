72b1167e8e392e56e747f24e8e34829adefa5588
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-22 21:34:29 -0400

    Used react-router-dom to set up routers for pages: home, products, and contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Contact from './components/pages/Contact';
+import Cart from './components/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Cart />
     </div>
   );
 }
