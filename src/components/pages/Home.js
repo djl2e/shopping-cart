e6e06ddc3a5e4d464d20329903b57fd8048d0c74
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-23 15:07:21 -0400

    Home page content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div className="home-info">
+        <p id="sub-home-text">Voted Best Phone Case of 2021</p>
+        <p id="main-home-text">Let Our Case Complete Your Fashion Phase</p>
+        <Link to="/products">
+          <button type="submit" id="shop-button">Shop Now</button>
+        </Link>
+      </div>
+      <img alt="main page" />
+    </div>
+  );
 }
 
 export default Home;
