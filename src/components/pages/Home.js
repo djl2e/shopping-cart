a1fe56f82ec9d470cebe48bb9882d972ef2725a1
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-24 12:35:58 -0400

    changed home page text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className="page home">
       <div className="home-info">
         <p id="sub-home-text">Voted Best Phone Case of 2021</p>
-        <p id="main-home-text">Let Our Phone Case Complete Your Fashion Phase</p>
+        <p id="main-home-text">Let our phone case be the end line of your fashion chase</p>
         <Link to="/products">
           <button type="submit" id="shop-button">Shop Now</button>
         </Link>
