5f0b13aa09db0593a649d1b691a82cb5ddd38e1f
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-27 18:34:31 -0400

    initial structure to cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import '../../css/Cart.css';
+import CartItem from './CartItem';
 
-function Cart() {
-  return <div />;
+function Cart(props) {
+  const {
+    isCartOpen, cartItems, decreaseQuantity, increaseQuantity, closeCart,
+  } = props;
+
+  let cartClassName = 'cart';
+  if (!isCartOpen) {
+    cartClassName = 'cart off';
+  }
+
+  const items = [];
+  for (let i = 0; i < cartItems.length; i++) {
+    const item = cartItems[i];
+    if (item[1] > 0) {
+      items.push(<CartItem
+        productId={item[0]}
+        quantity={item[1]}
+        decreaseQuantity={decreaseQuantity}
+        increaseQuantity={increaseQuantity}
+      />);
+    }
+  }
+
+  return (
+    <div className={cartClassName}>
+      <h2>Your Shopping Cart</h2>
+      <div className="cart-item-container">
+        {items}
+      </div>
+      <p>Total: $0.00</p>
+      <button type="submit" onClick={closeCart}>Checkout</button>
+      <button type="submit" onClick={closeCart}>Close</button>
+    </div>
+  );
 }
 
 export default Cart;
