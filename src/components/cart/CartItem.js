638f92a9eef33cbb3551f17e909ef3094df5c414
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-27 10:16:05 -0400

    cart item component

---
+++ src/components/cart/CartItem.js
@@ -0,0 +1,30 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+import React from 'react';
+import data from '../../data/data';
+
+function CartItem(props) {
+  const {
+    productId, quantity, decreaseQuantity, increaseQuantity,
+  } = props;
+  const decreaseButtonId = `decrease${productId}`;
+  const increaseButtonId = `increase${productId}`;
+  const productData = data[productId];
+
+  return (
+    <div className="cart-item">
+      <img src={require(`../../img/product${productId}.jpg`)} alt="cart item" />
+      <div className="cart-item-info">
+        <h3>{productData[0]}</h3>
+        <p>{productData[1]}</p>
+        <div className="quantity-container">
+          <button type="submit" className="decrease-button" id={decreaseButtonId} onClick={decreaseQuantity}>-</button>
+          <p>{quantity}</p>
+          <button type="submit" className="increase-button" id={increaseButtonId} onClick={increaseQuantity}>+</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
