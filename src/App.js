a7ad9fbc063463a8b8ba93d892992967fbb6043b
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-06-27 18:34:57 -0400

    new functions that deal with cart state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React, { useCallback, useState } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/jsx-no-bind */
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/pages/Home';
 import Products from './components/pages/products/Products';
 import Contact from './components/pages/Contact';
 import Cart from './components/cart/Cart';
+import data from './data/data';
 import './css/App.css';
 
 function App() {
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const numProducts = data.length;
 
-  const cartClicked = useCallback(() => {
-    setIsCartOpen(!isCartOpen);
-  }, [isCartOpen]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([...Array(numProducts).keys()].map((key) => [key, 0]));
+
+  let backgroundClassName = 'cart-background';
+  if (!isCartOpen) {
+    backgroundClassName = 'cart-background off';
+  }
+
+  function cartClicked() {
+    setIsCartOpen(true);
+  }
+
+  function closeCart() {
+    setIsCartOpen(false);
+  }
+
+  function decreaseQuantity(e) {
+    const targetProduct = e.target.id.slice(8);
+    if (cartItems[targetProduct]) {
+      const cartItemsCopy = [...cartItems];
+      const itemCopy = cartItemsCopy[targetProduct];
+      itemCopy[1] -= 1;
+      cartItemsCopy[targetProduct] = itemCopy;
+      setCartItems(cartItemsCopy);
+    }
+  }
+
+  function increaseQuantity(e) {
+    const targetProduct = e.target.id.slice(8);
+    const cartItemsCopy = [...cartItems];
+    const itemCopy = cartItemsCopy[targetProduct];
+    itemCopy[1] += 1;
+    cartItemsCopy[targetProduct] = itemCopy;
+    setCartItems(cartItemsCopy);
+  }
 
   return (
     <div className="container">
@@ -22,7 +57,14 @@ function App() {
         <Route path="products" element={<Products />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
-      <Cart />
+      <div className={backgroundClassName} onClick={closeCart} />
+      <Cart
+        isCartOpen={isCartOpen}
+        cartItems={cartItems}
+        decreaseQuantity={(e) => { decreaseQuantity(e); }}
+        increaseQuantity={(e) => { increaseQuantity(e); }}
+        closeCart={closeCart}
+      />
     </div>
   );
 }
